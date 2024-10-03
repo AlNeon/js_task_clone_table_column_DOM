be35347a58eaf7457dd42181b8f95234c70a2490
Oleksii Karpov <alneon1@gmail.com>
2024-10-03 16:31:27 +0300

    make build

---
+++ index.165e7a65.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("table tr").forEach(function(e){var t=e.querySelectorAll("td, th"),n=document.createElement(t[1].tagName);n.textContent=t[1].textContent,e.insertBefore(n,t[4])});
+//# sourceMappingURL=index.165e7a65.js.map
